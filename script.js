8b4501fc40779b82dac0139b4400fcc9972fadec
Spardutti <luisdamian.sp@gmail.com>
2021-08-13 20:37:25 -0300

    refactored everything

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { Game } from "./Functions/game";
-
 let config = {
   type: Phaser.AUTO,
   width: 800,
@@ -10,7 +8,22 @@ let config = {
       gravity: { y: 10 },
     },
   },
-  scene: [Game],
+  scene: { preload, create, update },
 };
 
+let player;
+let gameOver = false;
+
 let game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("grass", "assets/Grass_04.png");
+  this.load.image("player", "assets/player.png");
+}
+
+function create() {
+  this.add.image(400, 300, "grass");
+  player = this.add.sprite(200, 500, "player");
+}
+
+function update() {}
