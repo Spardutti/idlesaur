dad3df1c5c40cfeaea8d4b97c42e86d8d5b95cd5
ShegoS <jbarretolarrosa@gmail.com>
2021-08-15 20:52:17 -0300

    added attack prototype

--- src/NPCs/creep.js
+++ src/NPCs/creep.js
@@ -5,6 +5,8 @@ export default class Minotaur extends Phaser.GameObjects.Sprite {
     super(config.scene, config.x, config.y, "minotaur"); // scene, coordinadas, nombre de la imagen
     config.scene.add.existing(this);
     this.setInteractive();
+
+    //// ANIMATIONS
     this.anims.create({
       key: "minodead",
       frameRate: 5,
@@ -13,13 +15,23 @@ export default class Minotaur extends Phaser.GameObjects.Sprite {
         end: 49,
       }),
     });
+
+    //// EVENTS
     this.on("pointerup", function () {
       this.attack();
       Player.prototype.attack();
     })
-  }
 
+    //// STATS
+    this.hp = 10;
+    this.mp = 10;
+    
+  }
+  
+  ////METHODS
   attack() {
     this.play("minodead");
+    this.hp = this.hp - Player.prototype.pAtk
+    console.log(Player.prototype.pAtk, this.hp)
   }
 }
--- src/NPCs/player.js
+++ src/NPCs/player.js
@@ -1,13 +1,19 @@
 export default class Player extends Phaser.GameObjects.Sprite {
   constructor(config) {
-    super(config.scene, config.x, config.y, "player"); // scene, coordinadas, nombre de la imagen
+    super(config.scene, config.x, config.y, "player"); // scene, coordenadas, nombre de la imagen
     config.scene.add.existing(this);
     this.setInteractive();
+
+    //// EVENTS
     this.on("pointerdown", function () {
       this.attack();
     });
+
+    //// STATS
+    Player.prototype.pAtk = 1;
   }
 
+  //// METHODS
   attack() {
     console.log("attack");
   }
