84a23bbb1ad984e3477b262dad08ef9626e66cfe
Spardutti <luisdamian.sp@gmail.com>
2021-08-16 16:14:51 -0300

    player and npc classes

--- src/NPCs/Boss.js
+++ src/NPCs/Boss.js
@@ -1,12 +1,11 @@
-// MODIFICAR
-
-export default class Unit extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y, texture, frame, hp, damage) {
+export default class Boss extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y, texture, frame, hp, damage, level) {
     super(scene, x, y, texture, frame, hp, damage, level);
-    Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame);
+    Phaser.GameObjects.Sprite.call(this, x, y, texture, frame);
     this.setInteractive();
     this.hp = hp;
     this.damage = damage;
+    this.level = level;
   }
   attack(target) {
     target.takeDamage(this.damage);
--- src/NPCs/Heroes.js
+++ src/NPCs/Heroes.js
@@ -1,8 +1,8 @@
 // ACTUALizAR HEROES
 
 export default class Unit extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y, texture, frame, hp, damage) {
-    super(scene, x, y, texture, frame, hp, damage);
+  constructor(scene, x, y, texture, frame, hp, damage, level, experience) {
+    super(scene, x, y, texture, frame, hp, damage, level, experience);
     Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame);
     this.setInteractive();
     this.hp = hp;
